565e5769f86721f5478c43a3a8ebf13280105c49
mzanella <mzanella@users.noreply.github.com>
2018-07-17 15:58:34 +0200

    Update bootstrap.sh

--- kubernetes/centos/bootstrap.sh
+++ kubernetes/centos/bootstrap.sh
@@ -1,36 +1,3 @@
-#!/usr/bin/env bash
-
-yum update -y
-
-yum install -y docker
-systemctl enable docker && systemctl start docker
-
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-setenforce 0
-yum install -y kubelet-1.9.9-0 kubeadm-1.9.9-0 kubectl1.9.9-0
-systemctl enable kubelet && systemctl start kubelet
-
-## For Kube-router, see https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/#pod-network
-cat <<EOF >  /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sysctl --system
-
-swapoff -a
-sed -i '/swap/d' /etc/fstab
-
-setenforce 0
-sed -i '/SELINUX=enforcing/c\SELINUX=disabled' /etc/selinux/config
-
 #!/bin/bash
 
 function check () {
