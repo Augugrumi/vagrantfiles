d0fdd837d6df625e87eaf5126c7366836388483f
Davide Polonio <poloniodavide@gmail.com>
2018-07-18 15:56:54 +0200

    Move package installation order

--- kubernetes/centos/bootstrap.sh
+++ kubernetes/centos/bootstrap.sh
@@ -17,12 +17,12 @@ function main () {
 
     update
     
-    install yum-utils device-mapper-persistent-data lvm2
+    install yum-utils
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     
     echo -e '[kubernetes]\nname=Kubernetes\nbaseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg' | sudo tee /etc/yum.repos.d/kubernetes.repo
     
-    install docker-ce yum-plugin-versionlock kubelet-1.9.9-0 kubectl-1.9.9-0 kubeadm-1.9.9-0
+    install docker-ce device-mapper-persistent-data lvm2 yum-plugin-versionlock kubelet-1.9.9-0 kubectl-1.9.9-0 kubeadm-1.9.9-0 
     sudo yum versionlock kubelet kubeadm kubectl docker
     sudo systemctl enable docker && sudo systemctl start docker
     
