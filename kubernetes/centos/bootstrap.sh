8e669b8b505a423b5554837074ba3bd6919215c6
Davide Polonio <poloniodavide@gmail.com>
2018-07-18 11:22:22 +0200

    Add systemop module and remove install function

--- kubernetes/centos/bootstrap.sh
+++ kubernetes/centos/bootstrap.sh
@@ -1,48 +1,11 @@
 #!/bin/bash
 
-function check () {
-    if [ "$1" -ne 0 ]
-    then
-        msg err "$2"
-        exit 1
-    fi
-}
-
-function set_dbg () {
-    if [ "$1" = "true" ]
-    then
-        set -x
-    else
-        set +x
-    fi
-}
-
-function msg () {
-    # 3 type of messages:
-    # - info
-    # - warn
-    # - err
-    local color=""
-    local readonly default="\033[m" #reset
-    if [ "$1" = "info" ]
-    then
-        color="\033[0;32m" #green
-    elif [ "$1" = "warn" ]
-    then
-        color="\033[1;33m" #yellow
-    elif [ "$1" = "err" ]
-    then
-        color="\033[0;31m" #red
-    fi
-
-    echo -e "$color==> $2$default"
-}
-
-function install () {
-    sudo yum install -y $1
-}
+. <(curl -s https://raw.githubusercontent.com/Polpetta/minibashlib/master/minibashlib.sh)
 
 function main () {
+
+    mb_load "systemop"
+    
     sudo setenforce 0
     sudo sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
     
