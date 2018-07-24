42085960cd671e3f226534ac272bbe9a30b2240f
mzanella <mzanella@users.noreply.github.com>
2018-07-24 12:08:02 +0200

    Update bootstrap.sh

--- kubernetes/centos/bootstrap.sh
+++ kubernetes/centos/bootstrap.sh
@@ -21,6 +21,9 @@ function main () {
 
     msg info "Updating the system"
     update
+    
+    sudo sh -c 'echo -e "rbd\nip_vs" >  /etc/modules-load.d/ip_vs.conf'
+    sudo sh -c 'echo -e "dm_snapshot\ndm_mirror\ndm_thin_pool" >  /etc/modules-load.d/gluster.conf'
 
     msg info "Installing yum-utils"
     install yum-utils
@@ -31,10 +34,10 @@ function main () {
     echo -e '[kubernetes]\nname=Kubernetes\nbaseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg' | sudo tee /etc/yum.repos.d/kubernetes.repo
 
     msg info "Installing docker and kubernetes"
-    install docker device-mapper-persistent-data lvm2 yum-plugin-versionlock kubelet-1.9.9-0 kubectl-1.9.9-0 kubeadm-1.9.9-0
+    install docker device-mapper-persistent-data lvm2 yum-plugin-versionlock kubelet kubectl kubeadm git nano
 
     msg info "Locking updates for kubernetes and docker components"
-    sudo yum versionlock kubelet kubeadm kubectl docker
+    sudo yum versionlock docker
     msg info "Setting up autostart"
     sudo systemctl enable docker && sudo systemctl start docker
     sudo systemctl start kubelet && sudo systemctl enable kubelet
